095be4a2a8581bff0537893a38429fb138993613
jocularvernacular <115045285+jocularvernacular@users.noreply.github.com>
2025-03-05 11:35:08 +0100

    VQE

--- Exercise1.py
+++ Exercise1.py
@@ -112,3 +112,98 @@ def measure_bell_state(repetitions=10000):
     job = backend.run(compiled_circuit, shots=repetitions)
     results = job.result().get_counts()
     return results
+
+### Part b)
+import matplotlib.pyplot as plt
+
+def EW_1(lambd):
+    return 2 + sqrt((3 * lambd - 2)**2 + (0.2 * lambd) ** 2)
+
+def EW_2(lambd):
+    return 2 - sqrt((3 * lambd - 2)**2 + (0.2 * lambd) ** 2)
+
+x = [i / 100 for i in range(100)]
+y_e1 = [EW_1(i) for i in x]
+y_e2 = [EW_2(i) for i in x]
+
+plt.plot(x, y_e1)
+plt.plot(x, y_e2)
+plt.legend(["E1", "E2"])
+#plt.show()
+
+### Part c)
+from qiskit import QuantumCircuit
+from qiskit.quantum_info import SparsePauliOp
+from qiskit.circuit import Parameter
+
+# Constants
+E_1 = 0
+E_2 = 4
+V_11 = 3
+V_22 = -3
+V_12 = 0.2
+V_21 = V_12
+
+ansatz = QuantumCircuit(1)
+ansatz.rx(Parameter("a"), 0)
+ansatz.ry(Parameter("b"), 0)
+
+def create_Hamiltonian(x=0, y=0, z=0, I=0):
+    pauli_terms = []
+    if x != 0:
+        pauli_terms.append(("X", x))
+    if y != 0:
+        pauli_terms.append(("Y", y))
+    if z != 0:
+        pauli_terms.append(("Z", z))
+    if I != 0:
+        pauli_terms.append(("I", z))
+    return SparsePauliOp.from_list(pauli_terms)
+
+# H_0
+eps = (E_1 + E_2) / 2
+omega = (E_1 - E_2) / 2
+H_0 = create_Hamiltonian(z=omega, I=eps)
+
+# H_1
+c = (V_11 + V_22) / 2
+omega_z = (V_11 - V_22) / 2
+omega_x = V_12
+H_1 = create_Hamiltonian(x=omega_x, z=omega_z, I=c)
+
+# Define the Hamiltonian as a function of lambda
+def H(lambd):
+    return H_0 + lambd * H_1
+
+#from qiskit.primitives import StatevectorEstimator
+from qiskit.primitives import StatevectorEstimator
+
+def expectation_value(hamiltonian, params):
+    bound_ansatz = ansatz.assign_parameters(params)
+    estimator = StatevectorEstimator()
+    job = estimator.run([(bound_ansatz, hamiltonian)])
+    result = job.result()
+    expectation_value = result[0].data.evs
+    print(expectation_value)
+    return expectation_value
+
+from qiskit_algorithms.optimizers import COBYLA
+optimizer = COBYLA(maxiter=100)
+initial_params = np.random.rand(2)
+
+def objective_function(params):
+    return expectation_value(hamiltonian, params)
+
+vals = []
+for var in x:
+    hamiltonian = H(var)
+    result = optimizer.minimize(objective_function, initial_params)
+    optimized_params = result.x
+    optimized_energy = result.fun
+
+    print("Optimized parameters:", optimized_params)
+    print("Optimized energy (ground state energy):", optimized_energy)
+    vals.append(optimized_energy)
+
+plt.plot(x, vals)
+plt.show()
